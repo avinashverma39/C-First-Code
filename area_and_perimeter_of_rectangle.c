3c22d6f70dac9be865810d1fa081fe8767ebd4ad
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-02-11 23:26:12 +0530

    add

--- area_and_perimeter_of_rectangle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    float length, width, area, perimeter;
-    printf("Enter length and width %");
-    scanf("%f%f", &length, &width);
-    printf("Area is : %.2f\n", area = length * width);
-    printf("Perimeter is : %.2f", perimeter = 2 * (length + width));
-}
