6b82b7666aeecdb6944cd6356d9b585749409dbd
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-02-12 22:49:44 +0530

    Create swap.c

---
+++ 2_swap_value/swap.c
@@ -0,0 +1,19 @@
+// Program to swap two values with using a temporary variable
+#include <stdio.h>
+int main()
+{
+    int num1, num2, temp;
+    printf("Enter the first number: ");
+    scanf("%f", &num1);
+    printf("Enter the second number: ");
+    scanf("%f", &num2);
+
+    // Swapping the values using a temporary variable
+
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    printf("After swapping, the first number is: %d\n", num1);
+    printf("After swapping, the second number is: %d\n", num2);
+    return 0;
+}
