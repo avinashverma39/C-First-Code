8ef48215d09a954e27805f45f1493e38e1645aa7
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-02-26 19:26:07 +0530

    calculato2

---
+++ 8_Condition/calculator2.c
@@ -0,0 +1,46 @@
+// Create a program to create a simple calculator that uses a "switch case" statement to perform basic arithmetic operations
+
+#include <stdio.h>
+int main()
+{
+
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    char oper;
+    printf("Enter the operator (+, -, *, /): ");
+    scanf(" %c", &oper);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    switch (oper = '+')
+    {
+    case '+':
+        printf("The sum is: %d\n", num1 + num2);
+        break;
+
+    case '-':
+        peintf("The difference is: %d\n", num1 - num2);
+        break;
+
+    case '*':
+        printf("The product is: %d\n", num1 * num2);
+        break;
+    case '/':
+        if (num2 != 0)
+        {
+            printf("The quotient is: %d\n", num1 / num2);
+        }
+        else
+        {
+            printf("Error: Division by zero is not allowed.\n");
+        }
+        break;
+
+    default:
+        printf("Invalid operator. Please use +, -, *, or /.\n");
+
+        break;
+    }
+    return 0;
+}
