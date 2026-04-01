6bc18d19f16d5171ad18b375c0ff2c048956e067
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-04-01 23:15:11 +0530

    Add some new code

--- 9_loop/Table.c
+++ 9_loop/Table.c
@@ -3,13 +3,41 @@
 
 int main()
 {
-    int num1;
-    printf("Enter a number to print its multiplication table: ");
-    scanf("%d", &num1);
+    // USING FOR LOOP....................
 
-    for (int i = 1; i <= 10; i++)
+    // int num1;
+    // printf("Enter a number to print its multiplication table: ");
+    // scanf("%d", &num1);
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d x %d = %d\n", num1, i, num1 * i);
+    // }
+
+    // USING WHILE LOOP..........
+
+    // int num, number = 1;
+
+    // printf("Enter a number to print its multiplication table: ");
+    // scanf("%d", &num);
+    // while (number < 11)
+    // {
+    //     printf("%d x %d = %d\n", num, number, num * number);
+    //     number++;
+    // }
+
+    // USING DO-WHILE LOOP............
+
+    int num, number = 1;
+    printf("Enter a number to print its multiplication table: ");
+    scanf("%d", &num);
+    do
     {
-        printf("%d x %d = %d\n", num1, i, num1 * i);
-    }
+
+        printf("%d x %d = %d\n", num, number, num * number);
+        number++;
+
+    } while (number < 11);
+
     return 0;
 }
