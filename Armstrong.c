6044e5592365a146ce372b34a136857d968407f1
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-04-05 22:45:42 +0530

    Add some new code

--- Armstrong.c
+++ Armstrong.c
@@ -4,19 +4,27 @@
 int main()
 {
     // BY ISING WHILE LOOP
-    int num, originalNum, remainder, n;
-    int result = 0;
+    int num, originalNum, remainder, n = 0;
+    float result = 0.0;
     printf("Enter an integer: ");
     scanf("%d", &num);
     originalNum = num;
-    while (originalNum > 0)
+    // store the number of digits of num in n
+    while (originalNum != 0)
+    {
+        originalNum /= 10;
+        ++n;
+    }
+    originalNum = num;
+    // calculate the sum of the nth power of its digits
+    while (originalNum != 0)
     {
         remainder = originalNum % 10;
         result += pow(remainder, n);
         originalNum /= 10;
     }
-
-    if (result == num)
+    // display the result
+    if ((int)result == num)
         printf("%d is an Armstrong number.", num);
     else
         printf("%d is not an Armstrong number.", num);
