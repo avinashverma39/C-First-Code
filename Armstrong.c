4d80d77151a5a0a336ccba4ca45ea5f8efe08844
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-04-05 22:14:26 +0530

    Add new code

---
+++ Armstrong.c
@@ -0,0 +1,13 @@
+// PROGRAM TO CHECK NUMBER IS ARMSTRONG OR NOT
+#include <stdio.h>
+int main()
+{
+
+    int num, num2 = 0, temp, digit, power;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+     while()
+
+    return 0;
+}
