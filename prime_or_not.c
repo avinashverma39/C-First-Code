94f5b9745824f3d55f3575c82854aafca750b7b5
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-04-06 23:16:15 +0530

    Add new code

---
+++ prime_or_not.c
@@ -0,0 +1,22 @@
+// Program to check if a number is prime or not
+#include <stdio.h>
+int main(){
+
+    int num, i, value = 0;
+    printf("Enter a number: ");\
+    scanf("%d", &num);
+    for(i = 2; i <= num/2; i++){
+        if(num % i == 0){
+            value = 1;
+            break;
+        }
+    }
+    if(value == 0 && num > 1){
+        printf("%d is a prime number.", num);
+    }
+    else{
+        printf("%d is not a prime number.", num);
+    }
+    return 0;
+    
+}
