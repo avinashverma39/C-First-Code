3e8872b1cada314cc1c386d6d3ac44083cfd5b53
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-04-06 23:22:25 +0530

    Add code

--- prime_or_not.c
+++ prime_or_not.c
@@ -3,10 +3,10 @@
 int main()
 {
 
-    int num, i, value = 0;
+    int num, value = 0;
     printf("Enter a number: ");
     scanf("%d", &num);
-    for (i = 2; i <= num / 2; i++)
+    for (int i = 2; i <= num / 2; i++)
     {
         if (num % i == 0)
         {
